0fb4428215a0ca317fe9c78f54016a136ef52ed4
Robot (beeEEeEEEEEEEEP) <robot@example.org>
2020-07-31 01:44:23 +0000

    working

--- terraform/modules/gcp/k8s-cluster/main.tf
+++ terraform/modules/gcp/k8s-cluster/main.tf
@@ -3,9 +3,9 @@ terraform {
 }
 
 locals {
-  cluster                       = "cluster-demo-${var.environment}"
-  net_name                      = "net-demo-${var.environment}"
-  subnet_name                   = "subnet-demo-${var.environment}"
+  cluster                       = "cluster-maestro-${var.environment}"
+  net_name                      = "net-maestro-${var.environment}"
+  subnet_name                   = "subnet-maestro-${var.environment}"
   subnet_pods_ip_range_name     = "ip-range-pods-${var.environment}"
   subnet_services_ip_range_name = "ip-range-services-${var.environment}"
 }
